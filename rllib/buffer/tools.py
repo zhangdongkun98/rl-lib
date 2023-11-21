212bd46532f0692a45aca63bcc0336f9a424a512
zhangdongkun98 <zhangdongkun98@gmail.com>
2023-11-21 10:48:16 +0800

    sth

--- rllib/buffer/tools.py
+++ rllib/buffer/tools.py
@@ -2,14 +2,5 @@
 from typing import List
 from ..basic import Data
 
+from rldev.data import stack_data
 
-def stack_data(datas: List[Data]):
-    data_keys = datas[0].keys()
-    result = {}
-    for key, i in zip(data_keys, zip(*datas)):
-        if isinstance(i[0], Data):
-            result[key] = stack_data(i)
-        else:
-            result[key] = i
-    result = Data(**result)
-    return result
